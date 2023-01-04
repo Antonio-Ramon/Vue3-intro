012d1e8eb55c0cfc8b7b617ff3063e147d08d1fc
Antonio-Ramon <antonio-ramon-dev@outlook.com>
2023-01-04 18:31:01 -0300

    Lesson-4 End

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image: './assets/images/socks_green.jpg',
+            image: './assets/images/socks_blue.jpg',
             url: 'https://vuejs.org/guide/quick-start.html#creating-a-vue-application',
+            inventory: 8,
+            onSale: true,
         }
     }
 })
