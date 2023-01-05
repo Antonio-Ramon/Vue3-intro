6b40a6a70a6d97939430af0a5c19310027eef014
antonio-ramon <antonio-ramon-dev@outlook.com>
2023-01-05 17:26:36 -0300

    Lesson-5 End

--- main.js
+++ main.js
@@ -8,9 +8,17 @@ const app = Vue.createApp({
             // onSale: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green' },
-                { id: 2235, color: 'blue' },
+                { id: 2234, color: 'Green' },
+                { id: 2235, color: 'Blue' },
+
+            ],
+            sizes: [
+                { label: 'P'},
+                { label: 'M'},
+                { label: 'G'},
+                { label: 'GG'},
             ]
+              
         }
     }
 })
