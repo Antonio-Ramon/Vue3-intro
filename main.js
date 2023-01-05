f63725bc52d33af025cb93847ecac79d23cef2f3
antonio-ramon <antonio-ramon-dev@outlook.com>
2023-01-05 18:17:24 -0300

    Lesson-6 ongoing

--- main.js
+++ main.js
@@ -1,24 +1,33 @@
 const app = Vue.createApp({
     data() {
         return {
+            cart: 0,
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             url: 'https://vuejs.org/guide/quick-start.html#creating-a-vue-application',
             inventory: 11,
-            // onSale: true,
+            onSale: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
+
             variants: [
                 { id: 2234, color: 'Green' },
                 { id: 2235, color: 'Blue' },
-
             ],
+
             sizes: [
                 { label: 'P'},
                 { label: 'M'},
                 { label: 'G'},
                 { label: 'GG'},
-            ]
-              
+            ],
         }
+    },
+    
+    methods: {
+        addToCart() {
+            this.cart += 1
+            this.inventory -=1
+        }, 
     }
+
 })
