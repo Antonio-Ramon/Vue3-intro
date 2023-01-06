34f9a986f0ec181fc3e4a261b694254f849d069a
antonio-ramon <antonio-ramon-dev@outlook.com>
2023-01-06 01:43:22 -0300

    Lesson-6 end, add new images

--- main.js
+++ main.js
@@ -5,13 +5,16 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             url: 'https://vuejs.org/guide/quick-start.html#creating-a-vue-application',
-            inventory: 11,
+            inventory: 15,
             onSale: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
 
             variants: [
-                { id: 2234, color: 'Green' },
-                { id: 2235, color: 'Blue' },
+                { id: 2234, color: 'Green', image: './assets/images/socks_green.jpg' },
+                { id: 2235, color: 'Blue', image: './assets/images/socks_blue.jpg' },
+                { id: 2236, color: 'Red', image: './assets/images/socks_red.jpg' },
+                { id: 2237, color: 'Brown', image: './assets/images/socks_brown.jpg' },
+                { id: 2238, color: 'Purple', image: './assets/images/socks_purple.jpg' },
             ],
 
             sizes: [
@@ -27,7 +30,17 @@ const app = Vue.createApp({
         addToCart() {
             this.cart += 1
             this.inventory -=1
-        }, 
-    }
+        },
+
+        removefromCart(){
+            this.cart -= 1
+            this.inventory +=1
+        },
+
+        updateImage(variantImage){
+            this.image = variantImage
+        }
+    },
+
 
 })
