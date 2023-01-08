ad381b5bd0174b286cf556c86d0ce2c3ed8ff154
antonio-ramon <antonio-ramon-dev@outlook.com>
2023-01-08 01:25:25 -0300

    Lesson-7 Ongoing

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             url: 'https://vuejs.org/guide/quick-start.html#creating-a-vue-application',
-            inventory: 15,
+            inventory: 11,
             onSale: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
 
